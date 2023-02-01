66c6072a462a46065d394b08e4e2d366fd484e01
Jordan Last <jordan.michael.last@gmail.com>
2023-02-01 17:24:45 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { $query, $update } from 'azle';
+
+// This is a global variable that is stored on the heap
+let message: string = '';
+
+// Query calls complete quickly because they do not go through consensus
+$query;
+export function get_message(): string {
+    return message;
+}
+
+// Update calls take a few seconds to complete
+// This is because they persist state changes and go through consensus
+$update;
+export function set_message(new_message: string) {
+    message = new_message; // This change will be persisted
+}
+
