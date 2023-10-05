2cd116b4f1e17fb882c884560e399c89b1382c92
Jordan Last <jordan.michael.last@gmail.com>
2023-10-04 21:12:08 -0500

    update to Azle 0.18.0

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Welcome to your first Azle project! This example project will help you to deploy your first canister (application) to the Internet Computer (IC) decentralized cloud. It is a simple getter/setter canister. You can always refer to [The Azle Book](https://demergent-labs.github.io/azle/) for more in-depth documentation.
 
-This example assumes that you are using a \*nix environment (Linux, Mac OS, [WSL if using Windows](https://learn.microsoft.com/en-us/windows/wsl/install)) with bash and that you have [Node.js/npm installed](./installation.md#nodejs).
-
 `dfx` is the tool you will use to interact with the IC locally and on mainnet. If you don't already have it installed:
 
 ```bash
@@ -32,8 +30,8 @@ npm run canister_deploy_local
 To call the methods on your canister:
 
 ```bash
-npm run canister_call_set_message
 npm run canister_call_get_message
+npm run canister_call_set_message
 ```
 
 If you run the above commands and then call `npm run canister_call_get_message` you should see:
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { $query, $update } from 'azle';
+import { Canister, query, text, update, Void } from 'azle';
 
 // This is a global variable that is stored on the heap
-let message: string = '';
+let message = '';
 
-// Query calls complete quickly because they do not go through consensus
-$query;
-export function get_message(): string {
-    return message;
-}
-
-// Update calls take a few seconds to complete
-// This is because they persist state changes and go through consensus
-$update;
-export function set_message(new_message: string): void {
-    message = new_message; // This change will be persisted
-}
+export default Canister({
+    // Query calls complete quickly because they do not go through consensus
+    getMessage: query([], text, () => {
+        return message;
+    }),
+    // Update calls take a few seconds to complete
+    // This is because they persist state changes and go through consensus
+    setMessage: update([text], Void, (newMessage) => {
+        message = newMessage; // This change will be persisted
+    })
+});
 
